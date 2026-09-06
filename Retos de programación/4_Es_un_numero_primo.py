70d2626d35f8e5b220f22c57a6021bdc6af73ad0
Chen <1667191065qq.cf@gmail.com>
2026-09-06 18:47:49 +0200

    Add files via upload

--- Retos de programación/4_Es_un_numero_primo.py
+++ Retos de programación/4_Es_un_numero_primo.py
@@ -2,4 +2,23 @@
 #Como defino para que sea divisible entre 1 e i mismo que no haya otro numero mas 
 
 
-for i in range(2,100):
+for i in range(2,100): 
+    
+    es_primo=True
+    
+    for j in range(2 , i): #no hay la situacion de i=2 y j=2 
+                           #Por que en range(2,2) no imprime nada
+        
+        if i % j == 0: #Para un numero de i tenemos una lista de j que comprobar
+            
+            es_primo = False
+            break 
+        
+    if es_primo == True: # se ejecuta solo cuando es true 
+        
+        print(i, "es primo")
+        
+#(nada en i=2)
+#2         ← en i=3
+#2 3       ← en i=4
+#2 3 4     ← en i=5
